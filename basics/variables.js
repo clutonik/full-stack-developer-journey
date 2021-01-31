115396ce1cd4ec8f6b15efa29071d2445d1b9326
Sahil Chawla <sahil.chawla@clutonik.ca>
2021-01-30 21:56:04 -0500

    Basics about Variables

--- basics/variables.js
+++ basics/variables.js
@@ -1,5 +1,13 @@
 /*
-  javascript file to demonstrate use of variables
+  javascript file to demonstrate:
+  - Syntax to declare a variable
+  - Using Comments
+  - Using console.log to log data/values
+  - Basic Data types
+  - Storing values of a different type in an existing variable
+  - String Concatenation
+  - Use of `typeof` operator to check the data type of a variable
+  This script file can be viewed in browser/editor but can only be executed once added to an HTML file through <script> tag.
 */
 
 // Variable Declaration
@@ -7,3 +15,24 @@ let testVariable = "Hello World!"; // Having semi-colon at the end is important 
 
 // Printing variable value in console log (Open Dev tools in your browser to check cosole logs)
 console.log(testVariable);
+
+// Basic data types in javascript
+// We can change data types of an existing variable any time we want.
+let freeVariable = "Hello coder!!"; // String Type
+
+// logging Data type of the variables
+console.log("Type of freeVariable: ".concat(typeof freeVariable));
+
+// Changing Types now
+freeVariable = 32; // Integer Type
+console.log("Type of freeVariable: ".concat(typeof freeVariable));
+freeVariable = true; // Boolean Type
+console.log("Type of freeVariable: ".concat(typeof freeVariable));
+
+// You can also declare a variable without initializing type which is known as undefined type
+let undefinedVariable;
+console.log("Type of undefinedVariable: ".concat(typeof undefinedVariable));
+
+// You can declare and initialize a variable with null value (null Object)
+let nullVariable = null;
+console.log("Type of nullVariable: ".concat(typeof nullVariable));
