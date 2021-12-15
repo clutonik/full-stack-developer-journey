89343bab4003c973bf948fb373f23fe18b6a80bd
Sahil Chawla <62490945+clutonik@users.noreply.github.com>
2021-12-14 20:01:02 -0500

    Remote exec (#17)
    cloud-init

--- basics/terraform/cloud-init/main.tf
+++ basics/terraform/cloud-init/main.tf
@@ -54,35 +54,15 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+# Cloud-init script
+data "template_file" "cloud_init" {
+  template = file("cloud-init.yml")
+}
+
 resource "aws_instance" "test" {
   ami             = "ami-0ed9277fb7eb570c9"
   instance_type   = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
   key_name        = "test-ec2-keypair"
-
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Waiting for SSH to be available...'",
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file(local.private_key_path)
-      host        = aws_instance.test.public_ip
-    }
-
-  }
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ${self.public_ip}, httpd.yml --private-key ~/.ssh/test-ec2-keypair.pem -u ec2-user"
-
-
-    # You can also specify environment variables if needed by ansible
-    # environment = {
-    #   KEY = "value"
-    #  }
-
-  }
-
+  user_data       = data.template_file.cloud_init.rendered
 }
